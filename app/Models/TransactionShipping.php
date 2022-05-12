07c3042d5ab22fcfe5e21aaed5c0e35a34229ef9
WilliBuli17 <buliwilli17@gmail.com>
2022-05-12 12:39:14 +0700

    set expedition truck, transaction shipping models commit

--- app/Models/TransactionShipping.php
+++ app/Models/TransactionShipping.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon; // Add new library
 
 class TransactionShipping extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'transaction_id',
+        'employee_id',
+        'expedition_truck_id'
+    ];
+
+    // New functions for Carbon
+    public function getCreatedAtAttribute()
+    {
+        if (!is_null($this->attributes['created_at'])) {
+            return Carbon::parse($this->attributes['created_at'])->format('Y-m-d H:i:s');
+        }
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        if (!is_null($this->attributes['updated_at'])) {
+            return Carbon::parse($this->attributes['updated_at'])->format('Y-m-d H:i:s');
+        }
+    }
 }
