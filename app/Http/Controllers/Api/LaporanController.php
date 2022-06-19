7d7e6dfda5984223fb7ea6f959b2b3572f2d1500
WilliBuli17 <buliwilli17@gmail.com>
2022-06-19 21:45:50 +0700

    fix bug

--- app/Http/Controllers/Api/LaporanController.php
+++ app/Http/Controllers/Api/LaporanController.php
@@ -23,25 +23,25 @@ class LaporanController extends Controller
                     (SUM(case when ph.history_category = \'in\' then (ph.total_price) ELSE 0 END) - SUM(case when ph.history_category = \'out\' then (ph.total_price) ELSE 0 END)) as total_harga_barang
                 from product_histories ph
                 right join (
-                    select \'January  \' as month UNION
-                    select \'February \' as month UNION
-                    select \'March    \' as month UNION
-                    select \'April    \' as month UNION
-                    select \'May      \' as month UNION
-                    select \'June     \' as month UNION
-                    select \'July     \' as month UNION
-                    select \'August   \' as month UNION
-                    select \'September\' as month UNION
-                    select \'October  \' as month UNION
-                    select \'November \' as month UNION
-                    select \'December \' as month
+                    SELECT \'January\'   AS month, 1  AS Bln UNION
+                    SELECT \'February\'  AS month, 2  AS Bln UNION
+                    SELECT \'March\'     AS month, 3  AS Bln UNION
+                    SELECT \'April\'     AS month, 4  AS Bln UNION
+                    SELECT \'May\'       AS month, 5  AS Bln UNION
+                    SELECT \'June\'      AS month, 6  AS Bln UNION
+                    SELECT \'July\'      AS month, 7  AS Bln UNION
+                    SELECT \'August\'    AS month, 8  AS Bln UNION
+                    SELECT \'September\' AS month, 9  AS Bln UNION
+                    SELECT \'October\'   AS month, 10 AS Bln UNION
+                    SELECT \'November\'  AS month, 11 AS Bln UNION
+                    SELECT \'December\'  AS month, 12 AS Bln
                 )
-                as m on (TO_CHAR(ph.history_date, \'Month\') = m.month)
+                as m on monthname(ph.history_date) = m.month
                 where ph.product_id = ' . $id . '
-                and EXTRACT(year from CAST(ph.history_date AS DATE)) = ' . $year . '
+                and YEAR(ph.history_date) = ' . $year . '
                 and ph.deleted_at is null
                 group by bulan
-                order by EXTRACT(month from TO_DATE(m.month, \'Month\'))
+                order by m.Bln
             ');
 
             if (count($laporan) > 0) {
@@ -77,7 +77,7 @@ class LaporanController extends Controller
         try {
             $laporan = DB::select('
                 select
-                    EXTRACT(year from CAST(ph.history_date AS DATE)) as tahun,
+                    YEAR(ph.history_date) as tahun,
                     SUM(case when ph.history_category = \'in\' then (ph.amount_of_product) ELSE 0 END) as barang_masuk,
                     SUM(case when ph.history_category = \'in\' then (ph.total_price) ELSE 0 END) as harga_barang_masuk,
                     SUM(case when ph.history_category = \'out\' then (ph.amount_of_product) ELSE 0 END) barang_keluar ,
@@ -86,7 +86,7 @@ class LaporanController extends Controller
                     (SUM(case when ph.history_category = \'in\' then (ph.total_price) ELSE 0 END) - SUM(case when ph.history_category = \'out\' then (ph.total_price) ELSE 0 END)) as total_harga_barang
                 from product_histories ph
                 where ph.product_id = ' . $id . '
-                and EXTRACT(year from CAST(ph.history_date AS DATE))  BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
+                and YEAR(ph.history_date) BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
                 and ph.deleted_at is null
                 group by tahun
                 order by tahun
@@ -132,24 +132,24 @@ class LaporanController extends Controller
                     SUM(t.grand_total_price) as grand_total_price
                 from transactions t
                 right join (
-                    select \'January  \' as month UNION
-                    select \'February \' as month UNION
-                    select \'March    \' as month UNION
-                    select \'April    \' as month UNION
-                    select \'May      \' as month UNION
-                    select \'June     \' as month UNION
-                    select \'July     \' as month UNION
-                    select \'August   \' as month UNION
-                    select \'September\' as month UNION
-                    select \'October  \' as month UNION
-                    select \'November \' as month UNION
-                    select \'December \' as month
+                    SELECT \'January\'   AS month, 1  AS Bln UNION
+                    SELECT \'February\'  AS month, 2  AS Bln UNION
+                    SELECT \'March\'     AS month, 3  AS Bln UNION
+                    SELECT \'April\'     AS month, 4  AS Bln UNION
+                    SELECT \'May\'       AS month, 5  AS Bln UNION
+                    SELECT \'June\'      AS month, 6  AS Bln UNION
+                    SELECT \'July\'      AS month, 7  AS Bln UNION
+                    SELECT \'August\'    AS month, 8  AS Bln UNION
+                    SELECT \'September\' AS month, 9  AS Bln UNION
+                    SELECT \'October\'   AS month, 10 AS Bln UNION
+                    SELECT \'November\'  AS month, 11 AS Bln UNION
+                    SELECT \'December\'  AS month, 12 AS Bln
                 )
-                as m on (TO_CHAR(t.created_at, \'Month\') = m.month)
+                as m on monthname(t.created_at) = m.month
                 where t.transaction_status_id in (5,6)
-                and EXTRACT(year from CAST(t.created_at AS DATE)) = ' . $year . '
+                and YEAR(t.created_at) = ' . $year . '
                 group by bulan
-                order by EXTRACT(month from TO_DATE(m.month, \'Month\'))
+                order by m.Bln
             ');
 
             if (count($laporan) > 0) {
@@ -185,14 +185,14 @@ class LaporanController extends Controller
         try {
             $laporan = DB::select('
                 select
-                    EXTRACT(year from CAST(t.created_at AS DATE)) as tahun,
+                    YEAR(t.created_at) as tahun,
                     SUM(t.subtotal_price) as subtotal_price,
                     SUM(t.tax) as tax,
                     SUM(t.shipping_cost) as shipping_cost,
                     SUM(t.grand_total_price) as grand_total_price
                 from transactions t
                 where t.transaction_status_id in (5,6)
-                and EXTRACT(year from CAST(t.created_at AS DATE))  BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
+                and YEAR(t.created_at)  BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
                 group by tahun
                 order by tahun
             ');
@@ -237,24 +237,24 @@ class LaporanController extends Controller
                     AVG(case when dt.status = \'fail\' then (dt.total_price) ELSE 0 END) as fails_price
                 from detail_transactions dt
                 right join (
-                    select \'January  \' as month UNION
-                    select \'February \' as month UNION
-                    select \'March    \' as month UNION
-                    select \'April    \' as month UNION
-                    select \'May      \' as month UNION
-                    select \'June     \' as month UNION
-                    select \'July     \' as month UNION
-                    select \'August   \' as month UNION
-                    select \'September\' as month UNION
-                    select \'October  \' as month UNION
-                    select \'November \' as month UNION
-                    select \'December \' as month
+                    SELECT \'January\'   AS month, 1  AS Bln UNION
+                    SELECT \'February\'  AS month, 2  AS Bln UNION
+                    SELECT \'March\'     AS month, 3  AS Bln UNION
+                    SELECT \'April\'     AS month, 4  AS Bln UNION
+                    SELECT \'May\'       AS month, 5  AS Bln UNION
+                    SELECT \'June\'      AS month, 6  AS Bln UNION
+                    SELECT \'July\'      AS month, 7  AS Bln UNION
+                    SELECT \'August\'    AS month, 8  AS Bln UNION
+                    SELECT \'September\' AS month, 9  AS Bln UNION
+                    SELECT \'October\'   AS month, 10 AS Bln UNION
+                    SELECT \'November\'  AS month, 11 AS Bln UNION
+                    SELECT \'December\'  AS month, 12 AS Bln
                 )
-                as m on (TO_CHAR(dt.created_at, \'Month\') = m.month)
+                as m on monthname(dt.created_at) = m.month
                 where dt.product_id = ' . $id . '
-                and EXTRACT(year from CAST(dt.created_at AS DATE)) = ' . $year . '
+                and YEAR(dt.created_at) = ' . $year . '
                 group by bulan
-                order by EXTRACT(month from TO_DATE(m.month, \'Month\'))
+                order by m.Bln
             ');
 
             if (count($laporan) > 0) {
@@ -290,14 +290,14 @@ class LaporanController extends Controller
         try {
             $laporan = DB::select('
                 select
-                    EXTRACT(year from CAST(dt.created_at AS DATE)) as tahun,
+                    YEAR(dt.created_at) as tahun,
                     SUM(case when dt.status = \'success\' then (dt.amount_of_product) ELSE 0 END) as success,
                     AVG(case when dt.status = \'success\' then (dt.total_price) ELSE 0 END) as success_price,
                     SUM(case when dt.status = \'fail\' then (dt.amount_of_product) ELSE 0 END) as fails,
                     AVG(case when dt.status = \'fail\' then (dt.total_price) ELSE 0 END) as fails_price
                 from detail_transactions dt
                 where dt.product_id = ' . $id . '
-                and EXTRACT(year from CAST(dt.created_at AS DATE))  BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
+                and YEAR(dt.created_at) BETWEEN ' . $yearStart . ' AND ' . $yearEnd . '
                 group by tahun
                 order by tahun
             ');
@@ -339,21 +339,21 @@ class LaporanController extends Controller
                     SUM(case when dt.status = \'success\' then (dt.amount_of_product) ELSE 0 END) as success
                 from detail_transactions dt
                 right join (
-                    select \'January  \' as month, 1 AS bln UNION
-                    select \'February \' as month, 2 AS bln  UNION
-                    select \'March    \' as month, 3 AS bln  UNION
-                    select \'April    \' as month, 4 AS bln  UNION
-                    select \'May      \' as month, 5 AS bln  UNION
-                    select \'June     \' as month, 6 AS bln  UNION
-                    select \'July     \' as month, 7 AS bln  UNION
-                    select \'August   \' as month, 8 AS bln  UNION
-                    select \'September\' as month, 9 AS bln  UNION
-                    select \'October  \' as month, 10 AS bln  UNION
-                    select \'November \' as month, 11 AS bln  UNION
-                    select \'December \' as month, 12 AS bln
+                    SELECT \'January\'   AS month, 1  AS Bln UNION
+                    SELECT \'February\'  AS month, 2  AS Bln UNION
+                    SELECT \'March\'     AS month, 3  AS Bln UNION
+                    SELECT \'April\'     AS month, 4  AS Bln UNION
+                    SELECT \'May\'       AS month, 5  AS Bln UNION
+                    SELECT \'June\'      AS month, 6  AS Bln UNION
+                    SELECT \'July\'      AS month, 7  AS Bln UNION
+                    SELECT \'August\'    AS month, 8  AS Bln UNION
+                    SELECT \'September\' AS month, 9  AS Bln UNION
+                    SELECT \'October\'   AS month, 10 AS Bln UNION
+                    SELECT \'November\'  AS month, 11 AS Bln UNION
+                    SELECT \'December\'  AS month, 12 AS Bln
                 )
-                as m on (TO_CHAR(dt.created_at, \'Month\') = m.month)
-                where EXTRACT(year from CAST(dt.created_at AS DATE)) = ' . $year . '
+                as m on monthname(dt.created_at) = m.month
+                where YEAR(dt.created_at) = ' . $year . '
                 group by bln
                 order by bln
             ');
@@ -395,22 +395,22 @@ class LaporanController extends Controller
                     COUNT(t.id) as success
                 from transactions t
                 right join (
-                    select \'January  \' as month, 1 AS bln UNION
-                    select \'February \' as month, 2 AS bln  UNION
-                    select \'March    \' as month, 3 AS bln  UNION
-                    select \'April    \' as month, 4 AS bln  UNION
-                    select \'May      \' as month, 5 AS bln  UNION
-                    select \'June     \' as month, 6 AS bln  UNION
-                    select \'July     \' as month, 7 AS bln  UNION
-                    select \'August   \' as month, 8 AS bln  UNION
-                    select \'September\' as month, 9 AS bln  UNION
-                    select \'October  \' as month, 10 AS bln  UNION
-                    select \'November \' as month, 11 AS bln  UNION
-                    select \'December \' as month, 12 AS bln
+                    SELECT \'January\'   AS month, 1  AS Bln UNION
+                    SELECT \'February\'  AS month, 2  AS Bln UNION
+                    SELECT \'March\'     AS month, 3  AS Bln UNION
+                    SELECT \'April\'     AS month, 4  AS Bln UNION
+                    SELECT \'May\'       AS month, 5  AS Bln UNION
+                    SELECT \'June\'      AS month, 6  AS Bln UNION
+                    SELECT \'July\'      AS month, 7  AS Bln UNION
+                    SELECT \'August\'    AS month, 8  AS Bln UNION
+                    SELECT \'September\' AS month, 9  AS Bln UNION
+                    SELECT \'October\'   AS month, 10 AS Bln UNION
+                    SELECT \'November\'  AS month, 11 AS Bln UNION
+                    SELECT \'December\'  AS month, 12 AS Bln
                 )
-                as m on (TO_CHAR(t.created_at, \'Month\') = m.month)
+                as m on monthname(t.created_at) = m.month
                 where t.transaction_status_id in (5,6)
-                and EXTRACT(year from CAST(t.created_at AS DATE)) = ' . $year . '
+                and YEAR(t.created_at) = ' . $year . '
                 group by bln
                 order by bln
             ');
